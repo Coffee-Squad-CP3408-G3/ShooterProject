43754958072e778935d7da672ff2c05f6de9dcd9
Wxvxl <metal123cool1115@gmail.com>
2023-09-03 20:24:09 +0800

    Refactor enemy navigation script

--- Assets/Script/EnemyAI.cs
+++ Assets/Script/EnemyAI.cs
@@ -1,31 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-
-    public GameObject goal;
-    Vector3 direction;
-    public float speed = 5f;
-
+    NavMeshAgent enemy;
+    GameObject player;
     void Start()
     {
-
+        enemy = GetComponent<NavMeshAgent>();
         
+        player = GameObject.FindWithTag("Player");
     }
 
     private void LateUpdate()
     {
-        direction = goal.transform.position - transform.position;
-        transform.LookAt(goal.transform.position);
-        if (direction.magnitude > 2)
-        {
-            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
-            transform.position = transform.position + velocity;
-        }
-
-        //if (direction.magnitude > 15) 
-        //{
-        //    Vector3 velocity = new Vector3(0, 0, 0);
-        //}
+        enemy.SetDestination(player.transform.position);
     }
 }
