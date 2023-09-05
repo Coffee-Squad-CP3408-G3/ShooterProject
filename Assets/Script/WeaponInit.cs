ab7f212607db6c224ed569ff1d200bd69e2bfe91
Wxvxl <metal123cool1115@gmail.com>
2023-09-05 19:09:43 +0800

    Weapon switching added

--- Assets/Script/WeaponInit.cs
+++ Assets/Script/WeaponInit.cs
@@ -6,8 +6,30 @@ public class WeaponInit : MonoBehaviour
 {
     public GameObject[] weapon;
     public GameObject weaponPosition;
+    GameObject FirstGun;
+    GameObject SecondGun;
+    int ActiveWeapon;
     void Start()
     {
-        Instantiate(weapon[0], weaponPosition.transform.position, Quaternion.identity, weaponPosition.transform);
+        FirstGun = Instantiate(weapon[1], weaponPosition.transform.position, Quaternion.identity, weaponPosition.transform);
+        SecondGun = Instantiate(weapon[0], weaponPosition.transform.position, Quaternion.identity, weaponPosition.transform);
+        SecondGun.SetActive(false);
+        ActiveWeapon = 2;
+    }
+    
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.F)) {
+            if(ActiveWeapon == 2) {
+                FirstGun.SetActive(false);
+                SecondGun.SetActive(true);
+                ActiveWeapon = 1;
+            }
+
+            else {
+                SecondGun.SetActive(false);
+                FirstGun.SetActive(true);
+                ActiveWeapon = 2;
+            }
+        }
     }
 }
