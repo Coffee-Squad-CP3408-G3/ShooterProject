446bed3780fa56cac17f5a967f087d53b2cfdb5c
Jit Huang Brian Lim <jithuangbrian.lim@my.jcu.edu.au>
2023-09-11 14:51:32 +0800

    Player health registers enemy damage

--- Assets/Script/PlayerStats.cs
+++ Assets/Script/PlayerStats.cs
@@ -6,14 +6,45 @@ public class PlayerStats : MonoBehaviour
 {
     public AudioSource deadAudio;
     //Further stats to add: Movement speed, Damage Resist, Damage Multiplier, etc.
-    public float PlayerHealth = 1f;
+    public PlayerData playerData;
+    public float PlayerHealth = 100f;
+    float IFrameTimer = 0;
+
+    public void Start() {
+        playerData.PlayerInvincible = false;
+    }
+    public void Update() {
+        if (playerData.PlayerInvincible) {
+            IFrameTimer += Time.deltaTime;
+            Debug.Log(IFrameTimer);
+            if (IFrameTimer >= 1.5f) {
+                playerData.PlayerInvincible = false;
+                IFrameTimer = 0;
+            }
+        }
+    }
 
     public void PlayerTakeDamage(float EnemyDamage) {
-        PlayerHealth -= EnemyDamage;
-        if (PlayerHealth <= 0) {
-            deadAudio.Play();
-            Destroy(gameObject);
-            Debug.Log("You died");
+        if (!playerData.PlayerInvincible) {
+            PlayerHealth -= EnemyDamage;
+            Debug.Log("Player takes damage");
+            playerData.PlayerInvincible = true;
+            Debug.Log("IFrames true");
+
+            if (PlayerHealth < 100) {
+                Debug.Log("Less than 1000");
+            }
+            if (PlayerHealth < 50) {
+                Debug.Log("Less than 500");
+            }
+
+            if (PlayerHealth <= 0) {
+                deadAudio.Play();
+                Destroy(gameObject);
+                Debug.Log("You died");
+            }
         }
+
+        
     }
 }
--- Assets/Script/Scriptable Objects/PlayerData.cs
+++ Assets/Script/Scriptable Objects/PlayerData.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName="Soldier", menuName="Player/Soldier")]
 public class PlayerData : ScriptableObject {
     [Header("Affects Player Character")]
-    public float PlayerHealth;
+    public float PlayerHealthModifier;
     public float PlayerSpeedModifier;
     public float PlayerDamageResist;
+    public bool PlayerInvincible = false;
 
     [Header("Affects Gun")]
     public float PlayerDamageModifier;
     public int AmmoModifier;
     public float FireRateModifier;
-
 
 }
